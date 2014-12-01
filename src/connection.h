584cc7122e3d2dbcaab098116c248d1e1a2cd1ed
Symeon Huang <hzwhuang@gmail.com>
2014-12-01 22:53:35 +0000

    use the same buffer size as shadowsocks-python

--- src/connection.h
+++ src/connection.h
@@ -16,7 +16,8 @@ public:
     explicit Connection(QTcpSocket *localTcpSocket, QObject *parent = 0);
 
     qintptr socketDescriptor;
-    static const qint64 RecvSize = 16384;
+    static const qint64 RecvSize = 32736;//32KB, same as shadowsocks-python
+    static const qint64 RecvSize_udp = 65536;//64KB, same as shadowsocks-python
 
 public slots:
     void appendSocket(QTcpSocket *);
