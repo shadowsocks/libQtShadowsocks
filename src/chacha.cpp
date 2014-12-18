76cc9df01cd773fa195882c80c4ce6e05182c387
Symeon Huang <hzwhuang@gmail.com>
2014-12-18 22:09:59 +0000

    refine ChaCha. still need to change to be compatible.

--- src/chacha.cpp
+++ src/chacha.cpp
@@ -3,11 +3,20 @@
 
 using namespace QSS;
 
+#define LE(p) ((static_cast<quint32>((p)[0])) | (static_cast<quint32>((p)[1]) << 8) | (static_cast<quint32>((p)[2]) << 16) | (static_cast<quint32>((p)[3]) << 24))
+#define FROMLE(b, i) (b)[0] = i & 0xFF; (b)[1] = (i >> 8) & 0xFF; (b)[2] = (i >> 16) & 0xFF; (b)[3] = (i >> 24) & 0xFF;
+#define ROTL32(v, n) ((v) << (n)) | ((v) >> (32 - (n)))
+#define QUARTERROUND(x, a, b, c, d) \
+    x[a] += x[b]; x[d] = ROTL32(x[d] ^ x[a], 16); \
+    x[c] += x[d]; x[b] = ROTL32(x[b] ^ x[c], 12); \
+    x[a] += x[b]; x[d] = ROTL32(x[d] ^ x[a], 8); \
+    x[c] += x[d]; x[b] = ROTL32(x[b] ^ x[c], 7);
+
 ChaCha::ChaCha(const QByteArray &_key, const QByteArray &_iv, QObject *parent) :
     QObject (parent)
 {
-    const unsigned char *key = reinterpret_cast<const unsigned char*>(_key.constData());
-    const unsigned char *iv = reinterpret_cast<const unsigned char*>(_iv.constData());
+    const quint8 *key = reinterpret_cast<const quint8*>(_key.constData());
+    const quint8 *iv = reinterpret_cast<const quint8*>(_iv.constData());
 
     schedule.resize(16);
     keystream.resize(16);
@@ -28,54 +37,49 @@ ChaCha::ChaCha(const QByteArray &_key, const QByteArray &_iv, QObject *parent) :
     schedule[13] = 0;
     schedule[14] = LE(iv + 0);
     schedule[15] = LE(iv + 4);
-    available = 0;
+    chacha();
+    position = 0;
+}
+
+void ChaCha::chacha()
+{
+    keystream = schedule;
+    for (int i = 0; i < 10; ++i) {
+        QUARTERROUND(keystream.data(), 0, 4, 8, 12)
+        QUARTERROUND(keystream.data(), 1, 5, 9, 13)
+        QUARTERROUND(keystream.data(), 2, 6, 10, 14)
+        QUARTERROUND(keystream.data(), 3, 7, 11, 15)
+        QUARTERROUND(keystream.data(), 0, 5, 10, 15)
+        QUARTERROUND(keystream.data(), 1, 6, 11, 12)
+        QUARTERROUND(keystream.data(), 2, 7, 8, 13)
+        QUARTERROUND(keystream.data(), 3, 4, 9, 14)
+    }
+    for (int i = 0; i < 16; ++i) {
+        quint32 result = keystream[i] + schedule[i];
+        FROMLE(keystream.data() + i, result);
+    }
+    ++schedule[12];
+    schedule[13] += (schedule[12] == 0);
 }
 
 QByteArray ChaCha::update(const QByteArray &in)
 {
-    int length = in.length();
+    quint32 length = in.length();
     QByteArray out;
     out.resize(length);
     const quint8 *inpointer = reinterpret_cast<const quint8*>(in.data());
     quint8 *outpointer = reinterpret_cast<quint8*>(out.data());
 
-    if (available > 0) {
-        int amount = qMin(length, static_cast<int>(available));
-        chacha_xor(reinterpret_cast<quint8 *>(keystream.data()) + keystream.size() * 4 - available, inpointer, outpointer, amount);
-        available -= amount;
-        length -= amount;
-        inpointer += amount;
-        outpointer += amount;
+    for (quint32 delta = 64 - position; length >= delta; delta = 64 - position) {//keystream.size() * 4 = 64
+        chacha_xor(reinterpret_cast<quint8 *>(keystream.data()) + position, inpointer, outpointer, delta);
+        length -= delta;
+        inpointer += delta;
+        outpointer += delta;
+        chacha();
+        position = 0;
     }
 
-    while (length > 0) {
-        int amount = qMin(length, keystream.size() * 4);
-
-        keystream = schedule;
-        for (int i = 0; i < 10; ++i) {
-            QUARTERROUND(keystream.data(), 0, 4, 8, 12)
-            QUARTERROUND(keystream.data(), 1, 5, 9, 13)
-            QUARTERROUND(keystream.data(), 2, 6, 10, 14)
-            QUARTERROUND(keystream.data(), 3, 7, 11, 15)
-            QUARTERROUND(keystream.data(), 0, 5, 10, 15)
-            QUARTERROUND(keystream.data(), 1, 6, 11, 12)
-            QUARTERROUND(keystream.data(), 2, 7, 8, 13)
-            QUARTERROUND(keystream.data(), 3, 4, 9, 14)
-        }
-        for (int i = 0; i < 16; ++i) {
-            quint32 result = keystream[i] + schedule[i];
-            FROMLE(keystream.data() + i, result);
-        }
-
-        ++schedule[12];
-        schedule[13] += (schedule[12] == 0);
-
-        chacha_xor(reinterpret_cast<quint8 *>(keystream.data()), inpointer, outpointer, amount);
-        length -= amount;
-        inpointer += amount;
-        outpointer += amount;
-        available = keystream.size() * 4 - amount;
-    }
-
+    chacha_xor(reinterpret_cast<quint8 *>(keystream.data()) + position, inpointer, outpointer, length);
+    position += length;
     return out;
 }
--- src/chacha.h
+++ src/chacha.h
@@ -8,22 +8,13 @@
 
 namespace QSS {
 
-#define LE(p) ((static_cast<quint32>((p)[0])) | (static_cast<quint32>((p)[1]) << 8) | (static_cast<quint32>((p)[2]) << 16) | (static_cast<quint32>((p)[3]) << 24))
-#define FROMLE(b, i) (b)[0] = i & 0xFF; (b)[1] = (i >> 8) & 0xFF; (b)[2] = (i >> 16) & 0xFF; (b)[3] = (i >> 24) & 0xFF;
-#define ROTL32(v, n) ((v) << (n)) | ((v) >> (32 - (n)))
-#define QUARTERROUND(x, a, b, c, d) \
-    x[a] += x[b]; x[d] = ROTL32(x[d] ^ x[a], 16); \
-    x[c] += x[d]; x[b] = ROTL32(x[b] ^ x[c], 12); \
-    x[a] += x[b]; x[d] = ROTL32(x[d] ^ x[a], 8); \
-    x[c] += x[d]; x[b] = ROTL32(x[b] ^ x[c], 7);
-
 class ChaCha : public QObject
 {
     Q_OBJECT
 public:
     /*
      * Key length must be 32 (16 is dropped)
-     * iv is exactly the "nonce". we use IV to keep terminology consistency
+     * IV length must be 8
      */
     explicit ChaCha(const QByteArray &_key, const QByteArray &_iv, QObject *parent = 0);
 
@@ -34,7 +25,9 @@ public slots:
 private:
     QVector<quint32> schedule;//16
     QVector<quint32> keystream;//16
-    quint32 available;
+    quint32 position;
+
+    void chacha();
 
     inline void chacha_xor(quint8 *ks, const quint8 *in, quint8 *out, quint32 length)
     {
