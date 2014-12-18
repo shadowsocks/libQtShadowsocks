341fbd20afdf7d026ab1d740f8dae8e69a8baa9b
Symeon Huang <hzwhuang@gmail.com>
2014-12-18 22:12:03 +0000

    add license header.

--- src/chacha.cpp
+++ src/chacha.cpp
@@ -1,3 +1,25 @@
+/*
+ * chacha.cpp - the source file of ChaCha class
+ *
+ * Copyright (C) 2014, Symeon Huang <hzwhuang@gmail.com>
+ *
+ * This file is part of the libQtShadowsocks.
+ *
+ * libQtShadowsocks is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libQtShadowsocks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pdnsd; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #include "chacha.h"
 #include <QDebug>
 
