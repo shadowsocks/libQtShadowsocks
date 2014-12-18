20f695269179abbd63efa43943b1ec36377827a4
Symeon Huang <hzwhuang@gmail.com>
2014-12-18 21:47:06 +0000

    fix chacha.
    our ChaCha is not compatible with other ports. need more modifications.

--- src/chacha.cpp
+++ src/chacha.cpp
@@ -8,15 +8,14 @@ ChaCha::ChaCha(const QByteArray &_key, const QByteArray &_iv, QObject *parent) :
 {
     const unsigned char *key = reinterpret_cast<const unsigned char*>(_key.constData());
     const unsigned char *iv = reinterpret_cast<const unsigned char*>(_iv.constData());
-    static const QByteArray constants(32, 'k');
 
     schedule.resize(16);
     keystream.resize(16);
 
-    schedule[0] = LE(constants.data() + 0);
-    schedule[1] = LE(constants.data() + 4);
-    schedule[2] = LE(constants.data() + 8);
-    schedule[3] = LE(constants.data() + 12);
+    schedule[0] = 0x61707865;
+    schedule[1] = 0x3320646e;
+    schedule[2] = 0x79622d32;
+    schedule[3] = 0x6b206574;
     schedule[4] = LE(key + 0);
     schedule[5] = LE(key + 4);
     schedule[6] = LE(key + 8);
@@ -37,21 +36,23 @@ QByteArray ChaCha::update(const QByteArray &in)
     int length = in.length();
     QByteArray out;
     out.resize(length);
+    const quint8 *inpointer = reinterpret_cast<const quint8*>(in.data());
+    quint8 *outpointer = reinterpret_cast<quint8*>(out.data());
 
     if (available > 0) {
         int amount = qMin(length, static_cast<int>(available));
-        chacha_xor(reinterpret_cast<quint8 *>(keystream.data() + keystream.size() * 4 - available), reinterpret_cast<const quint8*>(in.data()), reinterpret_cast<quint8*>(out.data()), amount);
+        chacha_xor(reinterpret_cast<quint8 *>(keystream.data()) + keystream.size() * 4 - available, inpointer, outpointer, amount);
         available -= amount;
         length -= amount;
+        inpointer += amount;
+        outpointer += amount;
     }
 
     while (length > 0) {
         int amount = qMin(length, keystream.size() * 4);
 
-        quint32 *const nonce = schedule.data() + 12;
-        int i = 10;
         keystream = schedule;
-        while (i--) {
+        for (int i = 0; i < 10; ++i) {
             QUARTERROUND(keystream.data(), 0, 4, 8, 12)
             QUARTERROUND(keystream.data(), 1, 5, 9, 13)
             QUARTERROUND(keystream.data(), 2, 6, 10, 14)
@@ -61,19 +62,18 @@ QByteArray ChaCha::update(const QByteArray &in)
             QUARTERROUND(keystream.data(), 2, 7, 8, 13)
             QUARTERROUND(keystream.data(), 3, 4, 9, 14)
         }
-        for (i = 0; i < 16; ++i) {
+        for (int i = 0; i < 16; ++i) {
             quint32 result = keystream[i] + schedule[i];
             FROMLE(keystream.data() + i, result);
         }
-        if (!nonce[0] && !nonce[1] && !nonce[2]) {
-            ++nonce[0];
-            ++nonce[1];
-            ++nonce[2];
-            ++nonce[3];
-        }
 
-        chacha_xor(reinterpret_cast<quint8 *>(keystream.data()), reinterpret_cast<const quint8*>(in.data()), reinterpret_cast<quint8*>(out.data()), amount);
+        ++schedule[12];
+        schedule[13] += (schedule[12] == 0);
+
+        chacha_xor(reinterpret_cast<quint8 *>(keystream.data()), inpointer, outpointer, amount);
         length -= amount;
+        inpointer += amount;
+        outpointer += amount;
         available = keystream.size() * 4 - amount;
     }
 
