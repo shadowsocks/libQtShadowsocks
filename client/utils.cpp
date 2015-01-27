0d59a89bc2ffaab0c780e45f703107ba5ca3027f
Symeon Huang <hzwhuang@gmail.com>
2015-01-27 19:49:22 +0000

    use 32KB data for speed test.
    32KB is the biggest buffer that a TCP connection will take.

--- client/utils.cpp
+++ client/utils.cpp
@@ -5,13 +5,14 @@
 
 void Utils::testSpeed(const QString &method, quint32 data_size_mb)
 {
-    QByteArray test(1024 * 1024, '#');//1MB
+    QByteArray test(1024 * 32, '#');//32KB
+    quint32 loops = 32 * data_size_mb;
     QSS::Encryptor enc;
     enc.initialise(method, "barfoo!");
 
     QTime startTime = QTime::currentTime();
 
-    for (quint32 i = 0; i < data_size_mb; ++i) {//encrypt per 1MB to reduce memory usage during the test
+    for (quint32 i = 0; i < loops; ++i) {//encrypt per 1MB to reduce memory usage during the test
         enc.encrypt(test);
     }
 
@@ -28,5 +29,5 @@ void Utils::testSpeed(quint32 data_size_mb)
     }
 }
 
-const QStringList Utils::allMethods = QStringList() << "Table" << "AES-128-CFB" << "AES-192-CFB" << "AES-256-CFB" << "BF-CFB" << "CAST5-CFB" \
+const QStringList Utils::allMethods = QStringList() << "TABLE" << "AES-128-CFB" << "AES-192-CFB" << "AES-256-CFB" << "BF-CFB" << "CAST5-CFB" \
                                                     << "ChaCha20" << "DES-CFB" << "IDEA-CFB" << "RC2-CFB" << "RC4" << "RC4-MD5" << "Salsa20" << "SEED-CFB";
