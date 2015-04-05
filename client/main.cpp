0170fa85fab6f727bca1ee152b625b9720e551d5
Symeon Huang <hzwhuang@gmail.com>
2015-04-05 18:07:35 +0100

    mark 1.4.3

--- client/main.cpp
+++ client/main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     a.setApplicationName("Shadowsocks-libQtShadowsocks");
-    a.setApplicationVersion("1.4.2");
+    a.setApplicationVersion("1.4.3");
 
     signal(SIGINT, onSIGINT_TERM);
     signal(SIGTERM, onSIGINT_TERM);
