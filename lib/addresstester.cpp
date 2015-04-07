81a91759c0f11670fa2c763189d3d168c502d548
Symeon Huang <hzwhuang@gmail.com>
2015-04-07 13:44:26 +0100

    add comment

--- lib/addresstester.cpp
+++ lib/addresstester.cpp
@@ -1,3 +1,29 @@
+/*
+ * addresstester.cpp - the source file of AddressTester class
+ *
+ * Simple way to test the connection's latency.
+ * Since it's a just socket connection without any data transfer,
+ * the remote doesn't need to be a shadowsocks server.
+ *
+ * Copyright (C) 2014-2015 Symeon Huang <hzwhuang@gmail.com>
+ *
+ * This file is part of the libQtShadowsocks.
+ *
+ * libQtShadowsocks is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libQtShadowsocks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libQtShadowsocks; see the file LICENSE. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #include "addresstester.h"
 
 using namespace QSS;
